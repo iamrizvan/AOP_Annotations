8a955fb465b18dd0292b74295eee17d8ed363c5c
iamrizvan <rzkn000@gmail.com>
2020-04-28 23:06:19 +0400

    AOP After Before After-Returning After-Throwing Pointcut

---
+++ src/org/studyeasy/app/App.java
@@ -0,0 +1,25 @@
+package org.studyeasy.app;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.studyeasy.config.MyConfig;
+import org.studyeasy.service.Cars;
+
+
+public class App {
+
+	public static void main(String[] args) {
+     
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+		Cars cars = context.getBean("cars", Cars.class);
+		try {
+			cars.getPetrolCar().run();
+			context.close();
+		} catch (Exception e) {
+			System.out.println("Cought Exception "+ e.getMessage());
+			context.close();
+		}
+		
+	}
+	
+
+}
---
+++ src/org/studyeasy/cars/PetrolCar.java
@@ -0,0 +1,8 @@
+package org.studyeasy.cars;
+
+public class PetrolCar {
+	public void run() throws Exception {
+		System.out.println("running");
+		throw new Exception("What happend!");
+	}
+}
