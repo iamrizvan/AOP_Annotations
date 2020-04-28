9cd405365a6bf246dd1a885d62a8f64c6ebdd1b0
iamrizvan <rzkn000@gmail.com>
2020-04-29 00:01:28 +0400

    AOP Around Advice

--- src/org/studyeasy/aspects/Diagnose.java
+++ src/org/studyeasy/aspects/Diagnose.java
@@ -1,37 +1,32 @@
 package org.studyeasy.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Diagnose {
-
-	@Before("run()")
-	public void engine() {
-		System.out.println("Before Advice");
-	}
-	
-	@After("run()")
-	public void engine1() {
-		System.out.println("After Advice");
-	}
 	
 	@Pointcut("execution(void org.studyeasy.cars.PetrolCar.run())")
 	public void run() {	}
 	
-	@AfterReturning("run()")
-	public void afterReturning() {
-		System.out.println("After returning Advice");
-	}
-	
-	
-	@AfterThrowing("run()")
-	public void afterThrowing() {
-		System.out.println("After throwing Advice");
+	@Around("run()")
+	public void aroundAdvice(ProceedingJoinPoint jp)
+	{
+		System.out.println("Around advice intial message");
+		
+		try {
+			jp.proceed();
+		} catch (Throwable e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("Around advice later message");
 	}
 	
 }
